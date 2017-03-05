c352026a42f00c3a8a1e15ec49bdd7f0e9b79f79
Jk Jensen <azjkjensen@gmail.com>
2017-03-04 21:06:54 -0700

    Added some best fit line results.

--- concepts/contours.py
+++ concepts/contours.py
@@ -13,7 +13,24 @@ ret,thresh = cv2.threshold(imgray,127,255,0)
 # Find the contours using openCV
 im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 # Draw the contours for visual feedback
-c = cv2.drawContours(im, contours, -1, (0,255,0), 3)
+contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
+# print(list(contours[0]))
+# c = cv2.drawContours(im, contours, -1, (0,255,0), 3)
+rows,cols = im.shape[:2]
+[vx,vy,x,y] = cv2.fitLine((contours[0]), cv2.DIST_L2,0,0.01,0.01)
+print([vx,vy,x,y])
+lefty = int((-x*vy/vx) + y)
+righty = int(((cols-x)*vy/vx)+y)
+im = cv2.line(im,(cols-1,righty),(0,lefty),(0,255,0),1)
+
+# imageCenter = int(cols / 2)
+# bufferWidth = int(cols/20) # Buffer of "OK" image section (middle tenth of image)
+# figureCenter = int(lefty) + int((righty-lefty)/2)
+# if(figureCenter < imageCenter - bufferWidth:
+#   print("Move right!")
+# elif figureCenter > imageCenter + bufferWidth: 
+#   print("Move left!")
+
 # Print the diagnostic file for output
-cv2.imwrite(sys.argv[2], c)
+cv2.imwrite(sys.argv[2], im)
 
