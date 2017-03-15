5270fe6f39d106f6654ece761e9a34a35b6bdf92
Jk Jensen <azjkjensen@gmail.com>
2017-03-15 11:21:55 -0600

    Added description and removed unnecessary lines of code from moments script

--- concepts/moments.py
+++ concepts/moments.py
@@ -1,4 +1,10 @@
-# http://docs.opencv.org/3.2.0/d4/d73/tutorial_py_contours_begin.html
+'''
+This was a test to inspect the moments of a given image from its contours.
+
+This method was not selected because of the decentralizing of the image that using
+the moments would cause.
+'''
+
 
 import numpy as np
 import cv2
@@ -14,8 +20,6 @@ ret,thresh = cv2.threshold(imgray,127,255,0)
 im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 # Draw the contours for visual feedback
 contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
-# print(list(contours[0]))
-# c = cv2.drawContours(im, contours, -1, (0,255,0), 3)
 
 M = cv2.moments(contours[0])
 print(M)
@@ -28,21 +32,5 @@ im[cy+1][cx] = [0, 255, 0]
 im[cy][cx+1] = [0, 255, 0]
 im[cy+1][cx+1] = [0, 255, 0]
 print(im[cx][cy])
-# rows,cols = im.shape[:2]
-# [vx,vy,x,y] = cv2.fitLine((contours[0]), cv2.DIST_L2,0,0.01,0.01)
-# print([vx,vy,x,y])
-# lefty = int((-x*vy/vx) + y)
-# righty = int(((cols-x)*vy/vx)+y)
-# im = cv2.line(im,(cols-1,righty),(0,lefty),(0,255,0),1)
 
-# imageCenter = int(cols / 2)
-# bufferWidth = int(cols/20) # Buffer of "OK" image section (middle tenth of image)
-# figureCenter = int(lefty) + int((righty-lefty)/2)
-# if(figureCenter < imageCenter - bufferWidth:
-#   print("Move right!")
-# elif figureCenter > imageCenter + bufferWidth: 
-#   print("Move left!")
-
-# Print the diagnostic file for output
-#cv2.imwrite(sys.argv[2], im)
 
